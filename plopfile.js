c8f3474b0a7da8b4d7fe29af1a3e544fba3fcd7b
clamba15 <cl@customcraftsoftware.com>
2024-11-29 18:17:57 -0800

    cli

--- plopfile.js
+++ plopfile.js
@@ -8,17 +8,34 @@ export default function (plop) {
         type: 'list',
         name: 'name',
         message: 'Select the page you want to generate:',
-        choices: ['Home', 'About', 'Contact'],
+        choices: [
+          'Home',
+          'About',
+          'Contact',
+          'Services',
+          'Pricing',
+          'Careers',
+          'Error',
+          'Custom',
+        ],
       },
     ],
     actions: (data) => {
       const pageName = data.name;
+      let templateFile;
+
+      if (pageName === 'Custom') {
+        templateFile = 'Page.js.hbs';
+      } else {
+        templateFile = `${pageName}Page.js.hbs`;
+      }
+
       return [
         {
           type: 'add',
           path: `src/pages/{{pascalCase name}}Page.js`,
-          templateFile: path.resolve('templates/{{pascalCase name}}Page.js.hbs'),
-        }
+          templateFile: path.resolve(`templates/${templateFile}`),
+        },
       ];
     },
   });
