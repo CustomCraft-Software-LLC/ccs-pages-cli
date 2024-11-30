15a5d18f59a7576f14e1cb7866b074a76824f923
clamba15 <cl@customcraftsoftware.com>
2024-11-29 23:33:10 -0800

    cli

--- cli.js
+++ cli.js
@@ -7,25 +7,26 @@ const program = new Command();
 program
   .name('react-pages-cli')
   .description('CLI tool for generating React pages with MUI styling')
-  .version('1.0.0', '-v, --version', 'Output the current version');
+  .version('1.0.2', '-v, --version', 'Output the current version')
+  .helpOption('-h, --help', 'Display help for command');
 
 program
   .command('generate')
   .description('Generate a new React page with MUI styling')
   .action(async () => {
-    try {
-      const plopInstance = nodePlop(path.resolve('./plopfile.js'));
-      const generator = plopInstance.getGenerator('Page');
-
-      await generator.runActions({});
-      console.log(`Page generated successfully!`);
-    } catch (err) {
-      console.error('Error generating page:', err.message);
-    }
+    const plopInstance = nodePlop(path.resolve('./plopfile.js'));
+    const generator = plopInstance.getGenerator('Page');
+    await generator.runActions({});
+    console.log('Page generated successfully!');
   });
 
-program
-  .helpOption('-h, --help', 'Display help for command');
+program.on('--help', () => {
+  console.log('');
+  console.log('Examples:');
+  console.log('  $ react-pages-cli generate');
+  console.log('  $ react-pages-cli -v');
+  console.log('  $ react-pages-cli --help');
+});
 
 program.parse(process.argv);
 
